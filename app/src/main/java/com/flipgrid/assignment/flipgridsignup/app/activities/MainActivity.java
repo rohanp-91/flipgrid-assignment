3848edadf51aa6640114052fa5ad458c3d616f29
Rohan Pathak <ropathak@microsoft.com>
2022-04-21 01:02:10 -0700

    refactor activities

--- app/src/main/java/com/flipgrid/assignment/flipgridsignup/app/activities/MainActivity.java
+++ app/src/main/java/com/flipgrid/assignment/flipgridsignup/app/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.flipgrid.assignment.flipgridsignup;
+package com.flipgrid.assignment.flipgridsignup.app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.flipgrid.assignment.flipgridsignup.R;
 
 public class MainActivity extends AppCompatActivity {
 
