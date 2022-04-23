af5c8a0f1184544d347765ad11f689235a183e6e
Rohan Pathak <ropathak@microsoft.com>
2022-04-22 21:04:21 -0700

    use postDelayed for mocking

--- app/src/main/java/com/flipgrid/assignment/flipgridsignup/app/RegisterRequestService.java
+++ app/src/main/java/com/flipgrid/assignment/flipgridsignup/app/RegisterRequestService.java
@@ -5,58 +5,25 @@ import android.os.Looper;
 
 public class RegisterRequestService extends BaseRequestService {
 
-    private Handler mHandler;
-
-    public RegisterRequestService() {
-        super();
-        mHandler = new Handler(Looper.getMainLooper());
-    }
+    private final Handler mHandler = new Handler(Looper.getMainLooper());
 
     @Override
     public void getAsync(Object request, IResponseCallback responseCallback) {
-        Thread getAction = new Thread(new Runnable() {
+        mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                try {
-                    Thread.sleep(2000);
-                    mHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            responseCallback.onSuccess(request, new Object());
-                        }
-                    });
-                } catch (Exception e) {
-                    // do nothing for now
-                    e.printStackTrace();
-                }
+                responseCallback.onSuccess(request, new Object());
             }
-        });
-        getAction.start();
+        }, 2000);
     }
 
     @Override
     public void postAsync(Object request, IResponseCallback responseCallback) {
-        Thread postAction = new Thread(new Runnable() {
+        mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                try {
-                    Thread.sleep(2000);
-                    mHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            responseCallback.onSuccess(request, new Object());
-                        }
-                    });
-                } catch (Exception e) {
-                    // do nothing for now
-                    e.printStackTrace();
-                }
+                responseCallback.onSuccess(request, new Object());
             }
-        });
-        postAction.start();
-    }
-
-    public Handler getHandler() {
-        return mHandler;
+        }, 2000);
     }
 }
